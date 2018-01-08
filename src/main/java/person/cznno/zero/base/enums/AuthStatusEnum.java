9a966c6280a3f58393b6393ef27f0f8590e4c496
cznno <smeraldo.ash@gmail.com>
2018-01-08 17:49:29 +0800

    修改权限相关信息枚举

--- src/main/java/person/cznno/zero/base/enums/AuthStatusEnum.java
+++ src/main/java/person/cznno/zero/base/enums/AuthStatusEnum.java
@@ -14,7 +14,7 @@ public enum AuthStatusEnum implements BaseStatusEnum {
     LOGOUT_FAIL(false,"登出失败"),
 	REGISTER_SUCCESS(true,"注册成功"),
 	REGISTER_FAIL(false,"注册失败"),
-	REGISTER_REPEAT(false,"注册失败,重复注册");
+	REGISTER_REPEAT(false,"注册失败,用户重复");
 
     private Boolean success;
     private String msg;
