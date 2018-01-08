6263c8693e3c20cfe1b00fd3a223f4bdda08f8a5
cznno <smeraldo.ash@gmail.com>
2018-01-08 17:50:02 +0800

    修改返回实体工厂为静态工厂

--- src/main/java/person/cznno/zero/base/factory/BaseResponseFactory.java
+++ src/main/java/person/cznno/zero/base/factory/BaseResponseFactory.java
@@ -1,6 +1,7 @@
 package person.cznno.zero.base.factory;
 
 import person.cznno.zero.base.enums.BaseStatusEnum;
+import person.cznno.zero.base.enums.CrudStatusEnum;
 import person.cznno.zero.base.model.response.BaseResponse;
 import person.cznno.zero.base.model.response.Response;
 
@@ -9,20 +10,36 @@ import person.cznno.zero.base.model.response.Response;
  * Date: 17-12-28
  */
 public class BaseResponseFactory {
-
-    public static Response getResponse(BaseStatusEnum anEnum, Object data) {
+    public static Response get(Object data) {
         BaseResponse response = new BaseResponse();
-//        BaseStatusEnum commonStatusEnum = (BaseStatusEnum) anEnum;
-        response.setSuccess(anEnum.isSuccess());
-        response.setMsg(data.toString());
+        response.setSuccess(CrudStatusEnum.SELECT_SUCCESS.isSuccess());
+        response.setMsg(CrudStatusEnum.SELECT_SUCCESS.getMsg());
+        response.setData(data);
         return response;
     }
 
-    public Response getResponse(BaseStatusEnum anEnum) {
+    public static Response get(BaseStatusEnum anEnum, Object data) {
         BaseResponse response = new BaseResponse();
 //        BaseStatusEnum commonStatusEnum = (BaseStatusEnum) anEnum;
         response.setSuccess(anEnum.isSuccess());
         response.setMsg(anEnum.getMsg());
+        response.setData(data);
+        return response;
+    }
+
+    public static Response get(BaseStatusEnum anEnum) {
+        BaseResponse response = new BaseResponse();
+//        BaseStatusEnum commonStatusEnum = (BaseStatusEnum) anEnum;
+        response.setSuccess(anEnum.isSuccess());
+        response.setMsg(anEnum.getMsg());
+        return response;
+    }
+
+    public static Response get(Boolean success, String msg, Object data) {
+        BaseResponse response = new BaseResponse();
+        response.setSuccess(success);
+        response.setMsg(msg);
+        response.setData(data);
         return response;
     }
 }
---
+++ src/main/java/person/cznno/zero/base/factory/PagedResponseFactory.java
@@ -0,0 +1,58 @@
+package person.cznno.zero.base.factory;
+
+import com.github.pagehelper.PageInfo;
+import person.cznno.zero.base.enums.BaseStatusEnum;
+import person.cznno.zero.base.enums.CrudStatusEnum;
+import person.cznno.zero.base.model.response.BaseResponse;
+import person.cznno.zero.base.model.response.PagedResponse;
+import person.cznno.zero.base.model.response.Response;
+
+/**
+ * Created by cznno
+ * Date: 18-1-8
+ */
+public class PagedResponseFactory {
+
+    public static Response get(PageInfo data) {
+        PagedResponse response = new PagedResponse();
+        response.setSuccess(CrudStatusEnum.SELECT_SUCCESS.isSuccess());
+        response.setMsg(CrudStatusEnum.SELECT_SUCCESS.getMsg());
+        return setInfoData(response,data);
+    }
+
+    public static Response get(BaseStatusEnum anEnum, PageInfo data) {
+        PagedResponse response = new PagedResponse();
+        response.setSuccess(anEnum.isSuccess());
+        response.setMsg(anEnum.getMsg());
+        return setInfoData(response,data);
+    }
+
+    public static Response get(BaseStatusEnum anEnum) {
+        BaseResponse response = new BaseResponse();
+        response.setSuccess(anEnum.isSuccess());
+        response.setMsg(anEnum.getMsg());
+        return response;
+    }
+
+    public static Response get(Boolean success, String msg, PageInfo data) {
+        PagedResponse response = new PagedResponse();
+        response.setSuccess(success);
+        response.setMsg(msg);
+        return setInfoData(response,data);
+    }
+
+    public static Response get(Boolean success, String msg) {
+        BaseResponse response = new BaseResponse();
+        response.setSuccess(success);
+        response.setData(msg);
+        return response;
+    }
+
+    private static PagedResponse setInfoData(PagedResponse response, PageInfo data) {
+        response.setPage(data.getPageNum());
+        response.setRows(data.getPageSize());
+        response.setCount(data.getTotal());
+        response.setData(data.getList());
+        return response;
+    }
+}
